651fbbfcf21a5cdebcd9e2589cd5aefe2f6b2516
gq544 <woshigq513@163.com>
2019-07-04 16:43:35 +0800

    new

--- src/main/java/com/example/springbootrabbit/rabbitmqs/configs/RabbitConfig.java
+++ src/main/java/com/example/springbootrabbit/rabbitmqs/configs/RabbitConfig.java
@@ -5,6 +5,33 @@ import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ *
+ * 普通队列
+ * 这里注入一个AmqpTemplate来发布消息
+ * 接下来我们需要在rabbit-consumer工程配置一下消费者。
+ * 创建StringConsumer
+ */
+
+/**
+ * fanout模式
+ * fanout属于广播模式，只要跟它绑定的队列都会通知并且接受到消息。
+ * 我们同理在RabbitConfig中配置一下fanout模式的队列跟交换机。
+ * 在代码中我们配置了三个队列名、一个fanout交换机，并且将这三个队列绑定到了fanout交换器上。只要我们往这个交换机生产新的消息，那么这三个队列都会收到。
+ * 接下来，我们在RabbitProducer 中添加fanout的生产方法。
+ *
+ */
+
+/**
+ *
+ * topic模式，
+ * 同样，配置topic队列跟交换器，注意的是这里需要多配置一个bindingKey
+ * topicA的key为topic.msg 那么他只会接收包含topic.msg的消息
+ * topicB的key为topic.#那么他只会接收topic开头的消息
+ * topicC的key为topic.*.Z那么他只会接收topic.B.z这样格式的消息
+ * 同理在RabbitProducer完成topic生产方法
+ *
+ */
 @Configuration
 public class RabbitConfig {
     /**
